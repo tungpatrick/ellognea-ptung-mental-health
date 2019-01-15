d6d7927c53bd0466899d8ef562919839bab794c0
Patrick Tung <tung_patrick@yahoo.com>
2019-01-14 16:31:46 -0800

    updated clean_data to drop nas

--- src/data_clean.R
+++ src/data_clean.R
@@ -8,11 +8,12 @@ clean_data <- data %>%
   select("Age","Gender","Country","family_history","work_interfere","treatment","remote_work","benefits","seek_help","obs_consequence") %>%
   filter(Age>17 & Age<80)
 
+# drop na
+clean_data <- na.omit(clean_data)
+
 # sex
 clean_data <- clean_data %>% mutate(Gender = ifelse(Gender %in% c("Female", "female", "F", "f", "FEMALE"), "Female",
                                      ifelse(Gender %in% c("Male", "male","M","m","MALE","Mail", "maile", "Mal"), "Male", "Other")))
-# work interfere
-clean_data <- clean_data %>% mutate(work_interfere = ifelse(is.na(work_interfere), "None", work_interfere))
 
 # age group
 clean_data$age_group <- cut(clean_data$Age, breaks=c(18, 30, 40, 50, 60, 70, 80), right = FALSE, labels = FALSE)

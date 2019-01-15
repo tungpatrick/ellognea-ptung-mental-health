128c5084ef90965473021b9c36d75ff6e39beaf1
Patrick Tung <tung_patrick@yahoo.com>
2019-01-14 22:35:09 -0800

    updated app.R with interactivity + added to data_clean

--- Mental_Health_Explorer/app.R
+++ Mental_Health_Explorer/app.R
@@ -1,48 +1,59 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(tidyverse)
+library(rworldmap)
+library(plotly)
 #library(leaflet)
 
+# Read data
 data <- read_csv("../data/clean/data_clean.csv")
+data$X1 <- NULL
+treat_data <- read_csv("../data/clean/treatment_data.csv")
+treat_data$X1 <- NULL
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    
    # Application title
    titlePanel("Mental Health Explorer in the Workplace"),
+   tags$head(
+     tags$style(HTML("
+                     body {
+                      font-family: 'Helvetica';
+                     }
+                     "))
+     ),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         uiOutput("countryOutput")
-         
+        h3("Filters", align="center"),
+        br(),
+        uiOutput("countryOutput"),
+        width = 2
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("Age"),
-         plotOutput("Gender"),
-         plotOutput("family_history"),
-         plotOutput("work_interfere"),
-         plotOutput("remote_work"),
-         plotOutput("benefit"),
-         plotOutput("seek_help"),
-         plotOutput("obs_consequence"),
-         tableOutput(outputId = "table")
+        tabsetPanel(
+          tabPanel("Map", h3("Worldwide Mental Treatment Ratio", align="center"), 
+                   p("# of People who saught treatment / # of Respondents", align="center"),
+                   plotOutput("worldmap", height = "600px", width="auto")),
+          tabPanel("Plots", plotlyOutput("Age"), plotlyOutput("Gender"),
+                   plotlyOutput("family_history"), plotlyOutput("work_interfere"),
+                   plotlyOutput("remote_work"), plotlyOutput("benefit"), 
+                   plotlyOutput("seek_help"), plotlyOutput("obs_consequence")),
+          tabPanel("Data", tableOutput("table"))
+        )
       )
    )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+  output$worldmap <- renderPlot({
+    map <- joinCountryData2Map(treat_data, joinCode="NAME", nameJoinColumn="Country")
+    mapCountryData(map, nameColumnToPlot="treatment_ratio", colourPalette = "heat")
+  })
   
   output$countryOutput <- renderUI({
     selectInput('countryInput', 'Country', sort(unique(data$Country)), 
@@ -54,8 +65,8 @@ server <- function(input, output) {
       filter(Country %in% input$countryInput)
   )
   
-  output$Age <- renderPlot({
-    filtered_data() %>%  
+  output$Age <- renderPlotly({
+    age <- filtered_data() %>%  
       select(age_group, treatment) %>% 
       arrange(age_group,treatment) %>% 
       group_by(age_group,treatment) %>%
@@ -64,13 +75,13 @@ server <- function(input, output) {
       geom_bar(position="dodge", stat="identity")+
       ylab("Count")+
       xlab("Age groups")+
-      ggtitle("Mental heath treatment sought per age groups")+
+      ggtitle("Mental heath treatment sought per age groups") +
       theme_bw()
-    
+    ggplotly(age)
   })
    
-   output$Gender <- renderPlot({
-     filtered_data() %>%  
+   output$Gender <- renderPlotly({
+     gender <- filtered_data() %>%  
        select(Gender, treatment) %>% 
        arrange(Gender,treatment) %>% 
        group_by(Gender,treatment) %>%
@@ -80,11 +91,11 @@ server <- function(input, output) {
        ylab("Count")+
        ggtitle("Mental heath treatment sought per gender")+
        theme_bw()
-     
+     ggplotly(gender)
    })
    
-    output$family_history <- renderPlot({
-     filtered_data() %>%  
+    output$family_history <- renderPlotly({
+     fam_hist <- filtered_data() %>%  
        select(family_history, treatment) %>% 
        arrange(family_history,treatment) %>% 
        group_by(family_history,treatment) %>%
@@ -95,11 +106,12 @@ server <- function(input, output) {
        xlab("Family History")+
        ggtitle("Family history of mental illness and treatment sought")+
        theme_bw()
+     ggplotly(fam_hist)
      
    })
    
-   output$work_interfere <- renderPlot({
-     filtered_data() %>%  
+   output$work_interfere <- renderPlotly({
+     work_int <- filtered_data() %>%  
        select(work_interfere, treatment) %>% 
        arrange(work_interfere,treatment) %>% 
        group_by(work_interfere,treatment) %>%
@@ -110,10 +122,11 @@ server <- function(input, output) {
        xlab("Work interference")+
        ggtitle("Work inteference and treatment sought")+
        theme_bw()
+     ggplotly(work_int)
      
    })
    
-   output$remote_work <- renderPlot({
+   output$remote_work <- renderPlotly({
      filtered_data() %>%  
        select(remote_work, treatment) %>% 
        arrange(remote_work,treatment) %>% 
@@ -127,7 +140,7 @@ server <- function(input, output) {
        theme_bw()
    })
    
-   output$benefit <- renderPlot({
+   output$benefit <- renderPlotly({
      filtered_data() %>%  
        select(benefits, treatment) %>% 
        arrange(benefits,treatment) %>% 
@@ -141,7 +154,7 @@ server <- function(input, output) {
        theme_bw()
    })
    
-   output$seek_help <- renderPlot({
+   output$seek_help <- renderPlotly({
      
      filtered_data() %>%  
        select(seek_help, treatment) %>% 
@@ -156,7 +169,7 @@ server <- function(input, output) {
        theme_bw()
      
    })
-   output$obs_consequence<- renderPlot({
+   output$obs_consequence<- renderPlotly({
      
      filtered_data() %>%  
        select(obs_consequence, treatment) %>% 
